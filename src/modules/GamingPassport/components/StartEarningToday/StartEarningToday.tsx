a09435e2b2383ec47754a1ff3e1324c598ee7945
arturasxborg <124711605+arturasxborg@users.noreply.github.com>
2023-02-09 14:24:56 +0200

    change spots claimed

--- src/modules/GamingPassport/components/StartEarningToday/StartEarningToday.tsx
+++ src/modules/GamingPassport/components/StartEarningToday/StartEarningToday.tsx
@@ -15,8 +15,8 @@ export const StartEarningToday = () => {
   const titleRef = useRef(null);
   const scroll = new ScrollViewModel();
   const title = "Start Earning Today";
-  const spotsClaimed = 1000;
-  const maxSpots = 1000;
+  const spotsClaimed = 10000;
+  const maxSpots = 10000;
 
   function onAnimateIn() {
     const ref = titleRef.current;
@@ -32,111 +32,111 @@ export const StartEarningToday = () => {
 
   return (
     <div className={`start-earning-today expand row middle`}>
-      <div className='description expand row column middle'>
-        <ObserverContainer className='start' onAnimateIn={onAnimateIn}>
+      <div className="description expand row column middle">
+        <ObserverContainer className="start" onAnimateIn={onAnimateIn}>
           <h2>
-            <div className='base'>{title}</div>
-            <div className='scramble' ref={titleRef}>
+            <div className="base">{title}</div>
+            <div className="scramble" ref={titleRef}>
               {title}
             </div>
           </h2>
-          <div className='section-head-container'>
-            <p className='section-head-desc'>
+          <div className="section-head-container">
+            <p className="section-head-desc">
               Get rewarded for playing your favourite games.
             </p>
           </div>
         </ObserverContainer>
 
-        <div className='row column middle actions'>
-          <ButtonPrimary label='Get Early Access' />
+        <div className="row column middle actions">
+          <ButtonPrimary label="Get Early Access" />
 
-          <div className='plus' />
+          <div className="plus" />
 
-          <div className='giveaways'>In-game assets giveaways</div>
+          <div className="giveaways">In-game assets giveaways</div>
         </div>
-        <div className='wave-container row column'>
-          <div className='wave-details row between'>
-            <div className='wave'>
-              <div className='title'>All spots filled</div>
-              <div className='subtitle'>Closed Beta</div>
+        <div className="wave-container row column">
+          <div className="wave-details row between">
+            <div className="wave">
+              <div className="title">All spots filled</div>
+              <div className="subtitle">Closed Beta</div>
             </div>
-            <div className='spots'>
-              <div className='title'>Spots Claimed</div>
-              <div className='subtitle'>
+            <div className="spots">
+              <div className="title">Spots Claimed</div>
+              <div className="subtitle">
                 <span>{spotsClaimed.toLocaleString()}</span>/
                 {maxSpots.toLocaleString()}
               </div>
             </div>
           </div>
           <Progressbar progress={100} />
-          <div className='wave-ends row column middle center'>
-            <div className='title'>Public beta ends in:</div>
+          <div className="wave-ends row column middle center">
+            <div className="title">Public beta ends in:</div>
             <Countdown date={new Date("Feb 28, 2023 23:59:59")} />
           </div>
         </div>
       </div>
 
-      <div className='parallaxed-elements'>
-        <ObserverContainer className='parallaxed-right row middle center'>
+      <div className="parallaxed-elements">
+        <ObserverContainer className="parallaxed-right row middle center">
           <ParallaxLayer
             image={Images.gamingpassport.artifact}
             factorX={0.3}
             factorY={0.3}
             speed={0}
             isBase
-            className='parallaxed-1'
+            className="parallaxed-1"
           />
           <ParallaxLayer
             image={Images.gamingpassport.ghost}
             factorX={0}
             factorY={0}
             speed={0}
-            className='parallaxed-2'
+            className="parallaxed-2"
           />
         </ObserverContainer>
 
-        <ObserverContainer className='parallaxed-left row middle center'>
+        <ObserverContainer className="parallaxed-left row middle center">
           <ParallaxLayer
             image={Images.gamingpassport.blue_pet}
             factorX={-0.1}
             factorY={-0.1}
             speed={0}
             isBase
-            className='parallaxed-1'
+            className="parallaxed-1"
           />
           <ParallaxLayer
             image={Images.gamingpassport.counter_terrorist}
             factorX={0.1}
             factorY={0.1}
             speed={0}
-            className='parallaxed-2'
+            className="parallaxed-2"
           />
         </ObserverContainer>
       </div>
 
-      <div className='links row middle center'>
-        <Link to='/terms-and-agreements' onClick={onRedirect}>
+      <div className="links row middle center">
+        <Link to="/terms-and-agreements" onClick={onRedirect}>
           Privacy Policy
         </Link>
-        <Link to='/terms-and-agreements' onClick={onRedirect}>
+        <Link to="/terms-and-agreements" onClick={onRedirect}>
           Cookie Policy
         </Link>
-        <Link to='' onClick={onRedirect}>
+        <Link to="" onClick={onRedirect}>
           White Paper
         </Link>
-        <Link to='/launchpad' onClick={onRedirect}>
+        <Link to="/launchpad" onClick={onRedirect}>
           Launchpad
         </Link>
-        <Link to='/esports' onClick={onRedirect}>
+        <Link to="/esports" onClick={onRedirect}>
           Esports 3.0
         </Link>
-        <Link to='/xtreme-championship-series' onClick={onRedirect}>
+        <Link to="/xtreme-championship-series" onClick={onRedirect}>
           XCS
         </Link>
-        <Link to='/about-us' onClick={onRedirect}>
+        <Link to="/about-us" onClick={onRedirect}>
           About
         </Link>
-        <Link to='/faq' onClick={onRedirect}>
+        <Link to="/faq" onClick={onRedirect}>
           FAQ
         </Link>
       </div>
