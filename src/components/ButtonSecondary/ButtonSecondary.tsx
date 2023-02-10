34cca945f1e3b2d5dd67ddb73e6c943b5e697691
arturasxborg <124711605+arturasxborg@users.noreply.github.com>
2023-02-10 13:44:17 +0200

    fix secondary btn

--- src/components/ButtonSecondary/ButtonSecondary.tsx
+++ src/components/ButtonSecondary/ButtonSecondary.tsx
@@ -13,31 +13,31 @@ export const ButtonSecondary = ({
   onClick,
 }: ButtonSecondaryType) => {
   return (
-    <Link to={to} onClick={onClick} className='btn-secondary row middle'>
-      <div className='square' />
+    <a href={to} onClick={onClick} className="btn-secondary row middle">
+      <div className="square" />
       {label}
       <svg
-        width='16'
-        height='16'
-        viewBox='0 0 16 16'
-        fill='none'
-        xmlns='http://www.w3.org/2000/svg'
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d='M9.62 3.95312L13.6667 7.99979L9.62 12.0465'
-          strokeWidth='1.5'
-          strokeMiterlimit='10'
-          strokeLinecap='round'
-          strokeLinejoin='round'
+          d="M9.62 3.95312L13.6667 7.99979L9.62 12.0465"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
-          d='M2.33334 8H13.5533'
-          strokeWidth='1.5'
-          strokeMiterlimit='10'
-          strokeLinecap='round'
-          strokeLinejoin='round'
+          d="M2.33334 8H13.5533"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
-    </Link>
+    </a>
   );
 };
