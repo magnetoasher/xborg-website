d254ec5c1880e5c4d874ad80786d58bc90d5bb35
Louis Regis <louis@xborg.com>
2023-02-28 20:55:58 +0100

    Add https

--- src/modules/Seed/Seed.tsx
+++ src/modules/Seed/Seed.tsx
@@ -57,7 +57,7 @@ export const Seed = () => {
     form.timestamp = formattedDate;
 
     try {
-      const response = await fetch("http://gaming.xborg.com/api/seed/post", {
+      const response = await fetch("https://gaming.xborg.com/api/seed/post", {
         method: "POST",
         headers: {
           Accept: "application/json",
