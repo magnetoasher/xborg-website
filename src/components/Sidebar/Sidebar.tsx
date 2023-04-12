3fe6c486325003ea091a00bedec529673146f504
arturasxborg <arturas@xborg.com>
2023-04-12 18:29:09 +0300

    redirect logo

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { GlobalState } from "../../reducer";
 import { SiteActions } from "../../redux/site/actions";
 import { AppDispatch } from "../../store";
@@ -29,15 +30,17 @@ export const Sidebar = ({ active, total }: SidebarType) => {
 
   return (
     <div className={`page-sidebar ${loaded ? "is-loaded" : "is-loading"}`}>
-      <svg
-        viewBox="0 0 10 10"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="brand"
-      >
-        <path d="M9.38247 0V2.83234L6.73014 4.83048H2.65326L0 2.83234V0L4.69124 3.49281L9.38247 0Z" />
-        <path d="M9.38247 9.99992V7.16758L6.73014 5.16943H2.65326L0 7.16758V9.99992L4.69124 6.50711L9.38247 9.99992Z" />
-      </svg>
+      <Link to="/">
+        <svg
+          viewBox="0 0 10 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="brand"
+        >
+          <path d="M9.38247 0V2.83234L6.73014 4.83048H2.65326L0 2.83234V0L4.69124 3.49281L9.38247 0Z" />
+          <path d="M9.38247 9.99992V7.16758L6.73014 5.16943H2.65326L0 7.16758V9.99992L4.69124 6.50711L9.38247 9.99992Z" />
+        </svg>
+      </Link>
 
       <div className="scrollspy row column middle center">
         {totalArray.map((item: number) => (
