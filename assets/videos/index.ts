824a921437455585826b21f9e6d0d00c55acc896
arturasxborg <124711605+arturasxborg@users.noreply.github.com>
2023-10-10 01:37:02 -0700

    video encoding test

--- assets/videos/index.ts
+++ assets/videos/index.ts
@@ -1,6 +1,6 @@
 export const Videos = {
   landing: {
-    intro: './videos/Hero_section_withot_text.webm',
+    intro: './videos/Hero_section_withot_text.mp4',
   },
   lightning: './videos/lightning.webm',
   coin: './videos/Coin_Post_1_1.webm',
