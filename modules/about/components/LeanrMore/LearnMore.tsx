8a1942633e003592e277ba9c3decd9841eeac872
magnetoasher <xcaliburtriam@gmail.com>
2023-12-13 07:40:43 -0500

    updated styles

--- modules/about/components/LeanrMore/LearnMore.tsx
+++ modules/about/components/LeanrMore/LearnMore.tsx
@@ -1,19 +1,20 @@
 import Image from 'next/image';
 import { BtnDark, LinkButton } from '@/components/Buttons';
 import { SocialIcons } from '@/components/SocialIcons';
+import { FlashlightCursorWrapper } from '@/components/FlashlightCursorWrapper';
 
 export const LearnMore = () => {
   return (
     <div className="learn-more">
       <div className="inner-container">
-        <div className="card card1">
+        <FlashlightCursorWrapper className="card card1">
           <div className="card1-content">
             <p>COMING SOON...</p>
             <h4>$XBG is the native token of the XBorg ecosystem.</h4>
             <BtnDark className="card-btn" href="" label="Register Interest" />
           </div>
-        </div>
-        <div className="card card2">
+        </FlashlightCursorWrapper>
+        <FlashlightCursorWrapper className="card card2">
           <div className="card2-top">
             <h4>Prometheus NFT</h4>
             <p>Join the 1,111 Prometheans shaping the future of gaming.</p>
@@ -22,8 +23,8 @@ export const LearnMore = () => {
             <BtnDark className="card-btn" href="" label="View OpenSea" />
             <LinkButton text="Learn more" to="" />
           </div>
-        </div>
-        <div className="card card3">
+        </FlashlightCursorWrapper>
+        <FlashlightCursorWrapper className="card card3">
           <div className="card3-top">
             <h4>Join our Community</h4>
             <p>
@@ -34,9 +35,9 @@ export const LearnMore = () => {
           <div className="card3-bottom">
             <SocialIcons />
           </div>
-        </div>
+        </FlashlightCursorWrapper>
 
-        <div className="card card4">
+        <FlashlightCursorWrapper className="card card4">
           <h4>Built in Public</h4>
           <p>
             XBorg is built in transparency and decentralisation at its core.
@@ -47,7 +48,7 @@ export const LearnMore = () => {
             <LinkButton text="View Blog" to="" />
             <LinkButton text="View Roadmap" to="" />
           </div>
-        </div>
+        </FlashlightCursorWrapper>
       </div>
     </div>
   );
